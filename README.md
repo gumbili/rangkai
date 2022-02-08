84452f78ae2c9bb7dbab89a83f6fc21704336764
Rizky Kurniawan <contact.rizkykurniawan@gmail.com>
2022-02-09 04:40:26 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-## A PHP Layouting Library
+## A Minimalistic PHP Layouting Library
+
+> **Rangkai** is a layouting library extracted from a popular framework: CodeIgniter 4. **Rangkai** made to be reused for the process of layouting and rendering pages in the [BuahNaga web framework](https://github.com/gumbili/buahnaga).
+
+### Simple Usage
+```php
+<?php
+
+// Load the autoload file. You don't need to do this if you are installing Rangkai with composer install
+require_once './vendor/autoload.php';
+
+use Gumbili\Rangkai\Rangkai;
+
+// Create new Rangkai instance with provided views path where we store the view file.
+$rangkai = new Rangkai('Views');
+// Set some data to our view
+$rangkai->setData(['name' => 'Gumbili Project']);
+// Display our view with echo
+echo $rangkai->render('page');
+
+?>
+```
---
+++ src/ViewException.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Gumbili\Rangkai;
+
+use Exception;
+
+class ViewException extends Exception
+{
+    public static function forInvalidCellMethod(string $class, string $method)
+    {
+        return new static('Invalid cell method');
+    }
+
+    public static function forMissingCellParameters(string $class, string $method)
+    {
+        return new static('Missing cell parameters');
+    }
+
+    public static function forInvalidCellParameter(string $key)
+    {
+        return new static('Invalid cell parameter');
+    }
+
+    public static function forNoCellClass()
+    {
+        return new static('No cell class');
+    }
+
+    public static function forInvalidCellClass(string $class = null)
+    {
+        return new static('Invalid cell class');
+    }
+
+    public static function forTagSyntaxError(string $output)
+    {
+        return new static('Tag syntax error');
+    }
+
+    public static function forInvalidFile(string $path)
+    {
+        return new static('Invalid file: ' . $path);
+    }
+
+    public static function forInvalidArgumentException(string $message)
+    {
+        return new static('Invalid argument');
+    }
+}
